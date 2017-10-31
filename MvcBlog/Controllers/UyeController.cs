cdefb3b465d5f1002d375293f7a45996fc255b8f
azizKorkmaz <aziz_korkmaz21@hotmail.com>
2017-10-31 15:30:27 +0200

    üye düzenleme

--- MvcBlog/Controllers/UyeController.cs
+++ MvcBlog/Controllers/UyeController.cs
@@ -14,10 +14,92 @@ namespace MvcBlog.Controllers
         // GET: Uye
         MvcBlogDB db = new MvcBlogDB();
 
-        public ActionResult Index()
+        public ActionResult Index(int id)
+        {
+            var uye = db.Uyes.Where(u => u.UyeId == id).SingleOrDefault();
+            if (Convert.ToInt32(Session["uyeid"]) != uye.UyeId)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var uye = db.Uyes.Where(u => u.UyeId == id).SingleOrDefault();
+            if (Convert.ToInt32(Session["uyeid"]) != uye.UyeId)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Uye uye, int id, HttpPostedFileBase Foto)
+        {
+            if (ModelState.IsValid)
+            {
+                var uyes = db.Uyes.Where(u => u.UyeId == id).SingleOrDefault();
+                if (Foto != null)
+                {
+                    if (System.IO.File.Exists(Server.MapPath(uye.Foto)))
+                    {
+                        System.IO.File.Exists(Server.MapPath(uyes.Foto));
+                    }
+                    WebImage img = new WebImage(Foto.InputStream);
+                    FileInfo fotoinfo = new FileInfo(Foto.FileName);
+
+                    string newfoto = Guid.NewGuid().ToString() + fotoinfo.Extension;
+                    img.Resize(150, 150);
+                    img.Save("~/Uploads/UyeFoto/" + newfoto);
+                    uyes.Foto = "/Uploads/UyeFoto/" + newfoto;
+                }
+
+                uyes.AdSoyad = uye.AdSoyad;
+                uyes.KullaniciAdi = uye.KullaniciAdi;
+                uyes.Sifre = uye.Sifre;
+                uyes.Email = uye.Email;
+                db.SaveChanges();
+                Session["kullaniciadi"] = uye.KullaniciAdi;
+                return RedirectToAction("Index", "Home", new { id = uyes.UyeId });
+
+
+            }
+            return View();
+        }
+
+        public ActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(Uye uye)
+        {
+            var login = db.Uyes.Where(u => u.KullaniciAdi == uye.KullaniciAdi).SingleOrDefault();
+            if (login.KullaniciAdi == uye.KullaniciAdi && login.Email == uye.Email && login.Sifre == uye.Sifre)
+            {
+                Session["uyeid"] = login.UyeId;
+                Session["kullaniciadi"] = login.KullaniciAdi;
+                Session["yetkiid"] = login.YetkiID;
+                return RedirectToAction("Index", "Home");
+            }
+
+            else
+            {
+                ViewBag.uyari = "Kullanıcı Adı,Mail ya da Şifrenizi Kontrol Ediniz!";
+                return View();
+            }
+
+        }
+
+        public ActionResult Logout()
+        {
+            Session["uyeid"] = null;
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+
         public ActionResult Create()
         {
             return View();
@@ -27,7 +109,7 @@ namespace MvcBlog.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (Foto!=null)
+                if (Foto != null)
                 {
                     WebImage img = new WebImage(Foto.InputStream);
                     FileInfo fotoinfo = new FileInfo(Foto.FileName);
